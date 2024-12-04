e7695c1aa752e50a0ceb8b641c5aa1fa836b2170
Nuno <61714135+NunoR1@users.noreply.github.com>
2024-12-04 09:46:44 -0500

    Caesar Cypher

---
+++ Caesar Cypher/README.md
@@ -0,0 +1,3 @@
+A Caesar Cypher program that lets a user input a string, select by how much they want to shift the letters in the alphabet by, if they want to shift left or right, and if they want to encrypt or decrypt
+
+!! Make sure to download and place all files in the same folder
---
+++ Caesar Cypher/index.js
@@ -0,0 +1,74 @@
+// define varibles
+let userInput
+
+let output = document.getElementById("output") // the div where the encrypted and decrypted text while be output
+
+let submit = document.getElementById("submit")
+
+let shiftNum
+
+let shiftDir = document.getElementById("shiftDir")
+let shiftDirDisplay = document.getElementById("shiftDirDisplay")
+
+let convertType = document.getElementById("convertType")
+let convertTypeDisplay = document.getElementById("convertTypeDisplay")
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] // used to shift 
+
+function encrypt(text) {
+    let encryptedText = ""
+    for (let i = 0; i < text.length; i++) {
+        if (alphabet.indexOf(text[i]) >= 0) {
+            // adds the letter in alphabet at the index of the currently selected letter from the user input in alphabet added by the number of shifts; the module is to prevent overflow
+            encryptedText += alphabet[(alphabet.indexOf(text[i]) + shiftNum) % 26]
+        } else {
+            encryptedText += text[i] 
+        }
+    }
+    
+    return encryptedText
+}
+
+
+shiftDir.addEventListener("click", function() {
+    // changes the display of the words next to the button to the appropriate words
+    switch(shiftDirDisplay.textContent) {
+        case "Left":
+            shiftDirDisplay.textContent = "Right"
+            break
+        case "Right":
+            shiftDirDisplay.textContent = "Left"
+            break
+    }
+    
+    // if the direction of the shift is right the alphabet list will be reversed and returns it to normal when clicked again
+    alphabet.reverse()
+})
+
+
+convertType.addEventListener("click", function() {
+    // changes the display of the words next to the button to the appropriate words
+    switch(convertTypeDisplay.textContent) {
+        case "Encrypt":
+            convertTypeDisplay.textContent = "Decrypt"
+            submit.textContent = "Decrypt"
+            break
+        case "Decrypt":
+            convertTypeDisplay.textContent = "Encrypt"
+            submit.textContent = "Encrypt"
+            break
+    }
+    
+    // if the direction of the covert type is decrypt the alphabet list will be reversed and returns it to normal when clicked again
+    alphabet.reverse()
+})
+
+submit.addEventListener("click", function(event){
+    event.preventDefault()
+    // changes the values of the variable to the values of the input fields every time the button is clicked
+    shiftNum = parseInt(document.getElementById("shift#").value)
+    userInput = document.getElementById("theInput").value.toLowerCase()
+
+    output.textContent = encrypt(userInput)
+
+})
